397b6718b3d12ae1467210541d61eb73b1a0c878
Leonardo Natera <56647065+Mr-Leonerrr@users.noreply.github.com>
2023-01-29 22:44:51 -0500

    feat: add mongodb connection 👽

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 
 const Leoncito = require('./structures/Leoncito');
+const mongoose = require('mongoose');
 module.exports = client = new Leoncito();
 
 const init = async () => {
 	await require('./functions/handler')();
 	client.connect().catch((err) => console.log('Init error', err));
+
+	await mongoose.set('strictQuery', true);
+	await mongoose.default.connect(process.env.MONGODB_URI, {
+		dbName: 'leoncito',
+	}, () => console.log('MongoDB connected!'));
 };
 
 init().then(() => console.log('Bot is ready!'));
